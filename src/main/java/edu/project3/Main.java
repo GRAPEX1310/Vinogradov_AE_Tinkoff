793f4359f42553a2dee189f77d2a613e95919194
VinogradovAE <sashavin31@yandex.ru>
2023-11-18 19:56:24 +0300

    Исправление ошибок

--- src/main/java/edu/project3/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.project3;
-
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-
-public class Main {
-    CommandLineParser parser = new DefaultParser();
-}
